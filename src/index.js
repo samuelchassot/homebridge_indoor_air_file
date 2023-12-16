d67f004b999d5a88b379b77fbfe89093feb456a3
Samuel Chassot <samuel.chassot@epfl.ch>
2023-12-16 22:50:45 +0100

    working

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 "use strict";
 module.exports = function (homebridge) {
-  
+  /*
+        API.registerAccessory(PluginIdentifier,
+            AccessoryName, AccessoryPluginConstructor)
+    */
+  homebridge.registerAccessory("homebridge-indoor-air-http", "IndoorAirFile", indoorAir);
 };
