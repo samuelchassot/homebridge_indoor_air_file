02992bc5189daa85657e9955849b94cf8d03b0ff
Samuel Chassot <samuel.chassot@epfl.ch>
2023-12-17 10:55:01 +0100

    constructor

--- src/index.js
+++ src/index.js
@@ -4,5 +4,19 @@ module.exports = function (homebridge) {
         API.registerAccessory(PluginIdentifier,
             AccessoryName, AccessoryPluginConstructor)
     */
-  homebridge.registerAccessory("homebridge-indoor-air-http", "IndoorAirFile", indoorAir);
+  homebridge.registerAccessory("homebridge-indoor-air-http", "Indoor Air Information", indoorAir);
+};
+
+function indoorAir(log, config, api) {
+  this.log = log;
+  this.config = config;
+  this.homebridge = api;
+
+  if (this.config.url)
+      this.url = this.config.url;
+  else
+      this.url = "http://localhost:8080";
+
+  this.log('Indoor Air information plugin created!');
+  this.log('URL is ' + this.url);
 };
